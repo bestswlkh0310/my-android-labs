48b948ed8feb02de529dca1f7ff83784b1b82b75
bestswlkh0310 <hhhello0507@dgsw.hs.kr>
2024-06-07 10:44:09 +0900

    Intent

---
+++ Intent/app/src/main/java/com/bestswlkh0310/intent/Constant.kt
@@ -0,0 +1,5 @@
+package com.bestswlkh0310.intent
+
+object Constant {
+    const val TAG = "MY_LOG"
+}
---
+++ Intent/app/src/main/java/com/bestswlkh0310/intent/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.bestswlkh0310.intent
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var button: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        button = findViewById(R.id.btn_nav)
+        button.setOnClickListener {
+            val intent = Intent(this, DetailActivity::class.java)
+            intent.putExtra("myMessage", "야호")
+            startActivity(intent)
+        }
+    }
+}
